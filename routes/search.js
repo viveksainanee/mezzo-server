a2afdba2454b92b2cd964b7d39bb5806e4ef39f0
Vivek Sainanee <viveksainanee@gmail.com>
2019-01-24 20:03:55 -0800

    updated search route

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const axios = require('axios');
+const { YELP_API_KEY } = require('../secret');
 
 const router = express.Router();
 
+const api = axios.create({
+  baseURL: 'https://api.yelp.com/v3',
+  headers: {
+    Authorization: `Bearer ${YELP_API_KEY}`
+  }
+});
+
 /** GET search */
 
-router.get('', (req, res, next) => {
+router.get('', async function(req, res, next) {
   // do yelp call here
-  let result = [1, 2, 3];
-  return res.json(result);
+  let response = await api.get(`/businesses/search?location='Seattle'`);
+  return res.json(response.data.businesses);
 });
 
 module.exports = router;
